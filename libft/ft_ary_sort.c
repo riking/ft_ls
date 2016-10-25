100c16839797d64dd85abb39909200d06817af7d
Kane York <kyork@student.42.us.org>
2016-10-24 20:38:27 -0700

    update libft

--- libft/ft_ary_sort.c
+++ libft/ft_ary_sort.c
@@ -6,21 +6,19 @@
 /*   By: kyork <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/24 14:27:31 by kyork             #+#    #+#             */
-/*   Updated: 2016/10/24 15:08:05 by kyork            ###   ########.fr       */
+/*   Updated: 2016/10/24 20:36:01 by kyork            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <ft_printf.h>
 
-static void	sort(t_array *ary, size_t start, size_t len,
+static void		sort(t_array *ary, size_t start, size_t len,
 		int (*cmp)(void*, void*, size_t))
 {
 	size_t	lo_storage;
 	size_t	idx;
 	size_t	pivot_idx;
 
-	ft_printf("sort from %ld to %ld\n", start, start + len);
 	if (len <= 1)
 		return ;
 	pivot_idx = start;
@@ -31,7 +29,6 @@ static void	sort(t_array *ary, size_t start, size_t len,
 		if (cmp(ft_ary_get(ary, idx), ft_ary_get(ary, pivot_idx),
 					ary->item_size) <= 0)
 		{
-			ft_printf("swapping %ld and %ld", idx, lo_storage + 1);
 			ft_ary_swap(ary, idx, ++lo_storage);
 		}
 		idx++;
@@ -41,7 +38,7 @@ static void	sort(t_array *ary, size_t start, size_t len,
 	sort(ary, lo_storage + 1, len - (lo_storage - start) - 1, cmp);
 }
 
-void	ft_ary_sort(t_array *ary, int (*cmp)(void*, void*, size_t))
+void			ft_ary_sort(t_array *ary, int (*cmp)(void*, void*, size_t))
 {
 	sort(ary, 0, ary->item_count, cmp);
 }
